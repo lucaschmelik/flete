9a1c3bad5042fa6c9085d528fa06595545857beb
Lucas Chmelik <lchmelik@cys.com.ar>
2022-01-10 02:10:20 -0300

    Se crea estructura de la grilla de viajes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './Main';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
