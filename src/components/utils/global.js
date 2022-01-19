4efe55e5b8ede9b725efb814e74f6d50baee6f0f
Lucas Chmelik <lchmelik@cys.com.ar>
2022-01-19 09:22:03 -0300

    Se corrigen urls.

--- src/components/utils/global.js
+++ src/components/utils/global.js
@@ -1,13 +1,12 @@
 const ApiViajes = {
     urlViajes: "https://fleteapi.azurewebsites.net/api/viajes",
-    urlViajesPorFecha: "https://fleteapi.azurewebsites.net/api/fecha",
-    urlViajesPendientesPorFecha: "https://fleteapi.azurewebsites.net/api/pendientes",
-    urlViajesCompletadosPorFecha: "https://fleteapi.azurewebsites.net/api/completados",
-    urlViajeCompletado: "https://fleteapi.azurewebsites.net/api/entregado",
-    urlViajeDeshacer: "https://fleteapi.azurewebsites.net/api/deshacer",
-    urlViajeOrden: "https://fleteapi.azurewebsites.net/api/orden",
-    urlViajeEliminar: "https://fleteapi.azurewebsites.net/api/viajes/",
-    urlViajeCalendario: "https://fleteapi.azurewebsites.net/api/Calendario"
+    urlViajesPorFecha: "https://fleteapi.azurewebsites.net/api/viajes/fecha",
+    urlViajesPendientesPorFecha: "https://fleteapi.azurewebsites.net/api/viajes/pendientes",
+    urlViajesCompletadosPorFecha: "https://fleteapi.azurewebsites.net/api/viajes/completados",
+    urlViajeCompletado: "https://fleteapi.azurewebsites.net/api/viajes/entregado",
+    urlViajeDeshacer: "https://fleteapi.azurewebsites.net/api/viajes/deshacer",
+    urlViajeOrden: "https://fleteapi.azurewebsites.net/api/viajes/orden",
+    urlViajeCalendario: "https://fleteapi.azurewebsites.net/api/viajes/Calendario"
 }
 
 export default ApiViajes
